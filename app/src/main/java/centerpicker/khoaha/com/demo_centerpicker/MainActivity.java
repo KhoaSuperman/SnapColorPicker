6c6740c8e2e2e69bb5fdfd98543658b29840179f
Khoa <khoaha@teusoft.dk>
2016-01-18 11:07:13 +0700

    make item in list smaller

--- app/src/main/java/centerpicker/khoaha/com/demo_centerpicker/MainActivity.java
+++ app/src/main/java/centerpicker/khoaha/com/demo_centerpicker/MainActivity.java
@@ -1,15 +1,17 @@
 package centerpicker.khoaha.com.demo_centerpicker;
 
 import android.graphics.Point;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.Display;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int NUM_ITEMS = 5;
+    private static final int NUM_ITEMS = 20;
 
     private float itemWidth;
     private float padding;
@@ -38,7 +40,49 @@ public class MainActivity extends AppCompatActivity {
         shopItemslayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
         items.setLayoutManager(shopItemslayoutManager);
 
+        items.setOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                allPixels += dx;
+                Log.d(MyCons.LOG, "onScrolled allPixels: " + allPixels);
+            }
+
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                synchronized (this) {
+                    if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                        calculatePositionAndScroll(recyclerView);
+                    }
+                }
+            }
+        });
+
         ExtraItemsAdapter adapter = new ExtraItemsAdapter(NUM_ITEMS);
         items.setAdapter(adapter);
     }
+
+    private void calculatePositionAndScroll(RecyclerView recyclerView) {
+        int expectedPosition = Math.round((allPixels + padding - firstItemWidth) / itemWidth);
+        // Special cases for the padding items
+        if (expectedPosition < 0) {
+            expectedPosition = 0;
+        } else if (expectedPosition >= recyclerView.getAdapter().getItemCount() - 2) {
+            expectedPosition--;
+        }
+        scrollListToPosition(recyclerView, expectedPosition);
+    }
+
+    private void scrollListToPosition(RecyclerView recyclerView, int expectedPosition) {
+        float targetScrollPos = expectedPosition * itemWidth + firstItemWidth - padding;
+        float missingPx = targetScrollPos - allPixels;
+
+        //always check missingPx then scroll until missingPx = 0
+        if (missingPx != 0) {
+            recyclerView.smoothScrollBy((int) missingPx, 0);
+        }
+    }
+
 }
---
+++ app/src/main/java/centerpicker/khoaha/com/demo_centerpicker/MyCons.java
@@ -0,0 +1,8 @@
+package centerpicker.khoaha.com.demo_centerpicker;
+
+/**
+ * Created by HoangAnhKhoa on 1/18/16.
+ */
+public class MyCons {
+    public static final String LOG = "center picker";
+}
